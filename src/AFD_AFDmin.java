1372e0b70ade5aaa67904ad62c33f631e07b0382
BernardoElizondo <is708387@iteso.mx>
2019-05-13 14:38:40 -0500

    AFD_AFDMinimizado funcional. Falta realizar pruebas con otras entradas

--- src/AFD_AFDmin.java
+++ src/AFD_AFDmin.java
@@ -18,10 +18,12 @@ public class AFD_AFDmin {
 		ArrayList<Estado> estados = new ArrayList<Estado>();
 		ArrayList<ArrayList<String>> matriz = new ArrayList<ArrayList<String>>();
 		ArrayList<ArrayList<Estado>> tabla = new ArrayList<ArrayList<Estado>>();
+		ArrayList<ArrayList<Estado>> tablaN = new ArrayList<ArrayList<Estado>>();
 		
 		//Leemos el alfabeto y sacamos su magnitud
 		alfabeto = in.nextLine();
 		separarAlfabeto(alfabeto, alfabetoLista);
+		System.out.println("Size alfabeto: " + alfabetoLista.size());
 		
 		//Establecemos el estado inicial
 		estadoIni = Integer.parseInt(in.nextLine());
@@ -82,11 +84,98 @@ public class AFD_AFDmin {
 			if(tabla.get(i).get(0).esFinal == tabla.get(i).get(1).esFinal) {
 				
 			} else {
-				tabla.get(i).clear();
+				tabla.remove(i);
+				i--;
 			}
 		}
 		
+		System.out.println("Tabla" + tabla);
+		
+		
+		ArrayList<ArrayList<Estado>> borrados = new ArrayList<ArrayList<Estado>>();
+		ArrayList<ArrayList<String>> tablaPaso4 = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> tablaPaso4Copia = new ArrayList<ArrayList<String>>();
+		m = 0;
+		x = 0;
+		//Paso 4
+		while(m != 1) {
+			x = 0;
+			m = 1;
+
+			System.out.println("TablaPaso4Copia: " + tablaPaso4Copia);
+			System.out.println("TablaPaso4: " + tablaPaso4);
+			System.out.println("Borrados: " + borrados);
+			
+			
+			if(!borrados.isEmpty()) {
+				for(int i = 0; i < tablaPaso4Copia.size(); i++) {
+					for(int j = 0; j < (alfabetoLista.size()*2); j = j + 2) {
+						int estadoA = Integer.parseInt(tablaPaso4Copia.get(i).get(j));
+						int estadoB = Integer.parseInt(tablaPaso4Copia.get(i).get(j+1));
+						if(borrados.get(i).get(0).numEstado == estadoA && borrados.get(i).get(1).numEstado == estadoB) {
+							borrados.add(tabla.get(i));
+							tabla.remove(i);
+							tablaPaso4Copia.remove(i);
+							i--;
+							m = 0;
+							break;
+						}
+					}
+				}
+			}
+			for(int i = 0; i < tabla.size(); i++) {
+				int estadoA = tabla.get(i).get(0).numEstado;
+				int estadoB = tabla.get(i).get(1).numEstado;
+				
+				System.out.println(matriz.get(estadoA).get(0));
+				
+				ArrayList<String> filaLista = new ArrayList <String>();
+				for(int j = 0; j < alfabetoLista.size(); j++) {
+					filaLista.add(matriz.get(estadoA).get(j));
+					filaLista.add(matriz.get(estadoB).get(j));	
+				}		
+				tablaPaso4.add(filaLista);
+			}
+			for(int i = 0; i < tabla.size(); i++) {
+				for(int j = 0; j < tablaPaso4.get(i).size(); j = j + 2) {
+					int estadoA = Integer.parseInt(tablaPaso4.get(x).get(j));
+					int estadoB = Integer.parseInt(tablaPaso4.get(x).get(j+1));					
+					//System.out.println(i + "EstadoA: " + estadoA + " EstadoB: " + estadoB);
+					
+					if(estados.get(estadoA).esFinal != estados.get(estadoB).esFinal) {
+						borrados.add(tabla.get(i));
+						tabla.remove(i);
+						tablaPaso4.remove(x);
+						x--;
+						i--;
+						m = 0;
+						break;
+					} 
+				}
+				x++;
+			}
+			System.out.println("Matrix : " + tablaPaso4);
+			tablaPaso4Copia = (ArrayList<ArrayList<String>>) tablaPaso4.clone();
+			tablaPaso4.clear();
+			
+		}
+
+		
+		System.out.println("Tabla antes de prueba: " + tabla);
+		m = 1;
+		x = 0;
+		
+		
+		//Prueba
+
+
+		
+		System.out.println("Borrados: " + borrados);
+		
+
 		System.out.println(tabla);
+		
+		System.out.println("Matriz: " + matriz.toString());
 		
 		//System.out.println(tabla);
 		
